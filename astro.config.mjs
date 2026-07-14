9097a0b79aa921f33f36d0b06d27f341ace6f173
Lucy-Mazazuki <devojack8@gmail.com>
2026-07-13 22:36:17 -0500

    minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,3 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import github from "@astrojs/github";
-
-export default defineConfig({
-
-  output: "static",
-
-  adapter: github(),
-
-});
+export default defineConfig({});
